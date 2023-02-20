43f1cb6964a6fb0c553d02a61c3aa3768fed1d34
Kyaw Zin Thiha <hello@kyawzinthiha.com>
2023-02-20 22:05:27 +0000

    setup api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  	app.enableCors({
+		origin: process.env.CLIENT_URL || 'http://localhost:3000',
+		credentials: true,
+	});
+
+	app.use(helmet());
+	app.use(cookieParser(process.env.COOKIE_SECRET));
+  
+  const PORT = process.env.PORT || 3001;
+
+  await app.listen(PORT);
 }
 bootstrap();
